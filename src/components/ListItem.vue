c89422278f933beec70afdf5c91495d9b90e5fc9
Jack Carling <jackcarling@hotmail.com>
2024-04-01 19:37:10 +0200

    refactor: animation

--- src/components/ListItem.vue
+++ src/components/ListItem.vue
@@ -6,15 +6,20 @@ defineProps({
     width: {
         type: Number,
     },
+    index: {
+        type: Number,
+    },
 })
 </script>
 
 <template>
     <div
-        class="child"
+        class="child animate__animated animate__fadeInLeft"
         :style="{
             'min-width': `${width}px`,
+            'animation-delay': `${index * 0.2}s`,
         }"
+        @click="$router.push(`/show/${show['id']}`)"
     >
         <img
             :src="show['image']['medium']"
